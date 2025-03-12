b851a902bc3697f121af9f82abfcbcdb17ab9d2f
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-03-12 11:51:30 +0100

    [UPD] MAJ avec un Handle globale

--- client/src/pages/RepoForm.tsx
+++ client/src/pages/RepoForm.tsx
@@ -16,15 +16,21 @@ const initRepo = {
 function RepoForm(){
     const [newRepo,setNewRepo] = useState<Repos>(initRepo);
     console.log(newRepo);
-    
+
+    const HandleAddRepo = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewRepo(() => ({...newRepo, [e.target.name]: e.target.value}))
+    }
+
     return (
         <form>
             <h1>Ajout d'un nouveau Repo</h1>
             <label>
                 Description du Repo
-                <input type="text" name="description" value={newRepo?.description} onChange={(e) => {
-                    setNewRepo(() => ({...newRepo, [e.target.name]: e.target.value}))
-                }} />    
+                <input type="text" name="description" value={newRepo?.description} onChange={HandleAddRepo} />    
+            </label>
+            <label>
+                URL du Repo
+                <input type="text" name="url" value={newRepo?.url} onChange={HandleAddRepo} />    
             </label>
         </form>
     )
