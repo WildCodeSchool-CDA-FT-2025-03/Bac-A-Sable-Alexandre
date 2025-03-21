290861b19661e0b9c2a36e1b53f6b82ea33e0ce4
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-03-21 12:05:25 +0100

    Test var env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
+  console.log(import.meta.env.VITE_URL_BASE);
   return (
     <>
       <Header />
